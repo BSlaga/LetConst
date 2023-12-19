b130fc7c568705bd6269f84e3f9bdec5a04007ea
BSlaga <148803209+BSlaga@users.noreply.github.com>
2023-12-19 13:31:10 -0500

    Add files via upload

---
+++ letconst.js
@@ -0,0 +1,16 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+const PI = 3.14
+
+//What is the difference between var and let?
+//Let is a block and can't be redeclared, var is a function and can be reassigned and redeclared
+
+//What is the difference between var and const?
+//Const can't be reassigned and redeclared, var can do both 
+
+//What is the difference between let and const?
+//Let can just reassign, const can't reassign and redeclare
+
+//What is hoisting?
+//Interpreter moves declaration to the top of their scope, before the code runs
